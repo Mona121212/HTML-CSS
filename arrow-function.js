9dd7a02a442e1d3562df747d4bea69360a3979fb
Mona <xiaomei.mona.he@gmail.com>
2025-09-20 23:50:17 -0600

    feat: add array method

--- arrow-function.js
+++ arrow-function.js
@@ -5,3 +5,12 @@ console.log(greet("Mona"));
 
 const add = (a, b) => a + b;
 console.log(add(2,3));
+
+// array method
+const numbers = [1,2,3,4,5];
+
+const doubled = numbers.map(n => n*2);
+const evens = numbers.filter(n => n % 2 === 0);
+const found = numbers.find(n => n > 3);
+
+// create students manager system
