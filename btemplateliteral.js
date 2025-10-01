4ff164c82fab31e98bfb4d1bf069a12ac77f0f9d
Mona <xiaomei.mona.he@gmail.com>
2025-09-30 23:49:52 -0600

    feat: template literal

---
+++ btemplateliteral.js
@@ -0,0 +1,15 @@
+// ...
+
+
+const greeting = createGreeting('Joe Blow');
+
+function createGreeting(name) {
+    return `Hello, ${name}!`;
+}
+
+console.log(greeting);
+
+/* 
+ * Expected output:
+ * Hello Joe Blow!
+ */
